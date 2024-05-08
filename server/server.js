d69d93ce9d55faabf7f5134a6c5709b72c21167a
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2024-05-08 18:37:18 +0100

    correct-commit

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const url = process.env.MONGODB_URI
 //   }
   app.use(
     cors({
-    origin: "https://youshorten.vercel.app/",
+    origin: "https://youshorten.vercel.app",
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: [
