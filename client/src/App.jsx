21c2966da97f8fcec53eeb2ac03e66093111a674
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2024-05-08 16:59:57 +0100

    second-commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { url as api } from "./api/fetchUrl"
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const [url, setUrl] = useState("")
+  const [shorturl, setShortUrl] = useState("")
+  const [isLoading, setIsLoading] = useState(null)
+const shortenUrl = async () => {
+  setIsLoading(true)
+  try{
+const response = await api.post("/api/url/shorten")
+if(response && response.data){
+  setIsLoading(false)
+setShortUrl(response.data.shorturl)
+}
+  }catch(err){
+    setIsLoading(false)
+    console.log(err.name)
+    setShortUrl("Unable To Shorten Url")
+    console.log(err)
+  }
+}
+const submitForm = async (e) => {
+  e.preventDefault()
+shortenUrl()
+}
+    return (
+    <div>
+      <div className="login-root">
+        <div className="box-root flex-flex flex-direction--column" style={{ minHeight: '100vh', flexGrow: 1 }}>
+          <div className="loginbackground box-background--white padding-top--64">
+            <div className="loginbackground-gridContainer">
+              <div className="box-root flex-flex" style={{ gridArea: 'top / start / 8 / end' }}>
+                <div className="box-root" style={{ backgroundImage: 'linear-gradient(white 0%, rgb(247, 250, 252) 33%)', flexGrow: 1 }}></div>
+              </div>
+              <div className="box-root flex-flex" style={{ gridArea: '4 / 2 / auto / 5' }}>
+                <div className="box-root box-divider--light-all-2 animationLeftRight tans3s" style={{ flexGrow: 1 }}></div>
+              </div>
+              <div className="box-root flex-flex" style={{ gridArea: '6 / start / auto / 2' }}>
+                <div className="box-root box-background--blue800" style={{ flexGrow: 1 }}></div>
+              </div>
+              <div className="box-root flex-flex" style={{ gridArea: '7 / start / auto / 4' }}>
+                <div className="box-root box-background--blue animationLeftRight" style={{ flexGrow: 1 }}></div>
+              </div>
+              <div className="box-root flex-flex" style={{ gridArea: '8 / 4 / auto / 6' }}>
+                <div className="box-root box-background--gray100 animationLeftRight tans3s" style={{ flexGrow: 1 }}></div>
+              </div>
+              <div className="box-root flex-flex" style={{ gridArea: '2 / 15 / auto / end' }}>
+                <div className="box-root box-background--cyan200 animationRightLeft tans4s" style={{ flexGrow: 1 }}></div>
+              </div>
+              <div className="box-root flex-flex" style={{ gridArea: '3 / 14 / auto / end' }}>
+                <div className="box-root box-background--blue animationRightLeft" style={{ flexGrow: 1 }}></div>
+              </div>
+              <div className="box-root flex-flex" style={{ gridArea: '4 / 17 / auto / 20' }}>
+                <div className="box-root box-background--gray100 animationRightLeft tans4s" style={{ flexGrow: 1 }}></div>
+              </div>
+              <div className="box-root flex-flex" style={{ gridArea: '5 / 14 / auto / 17' }}>
+                <div className="box-root box-divider--light-all-2 animationRightLeft tans3s" style={{ flexGrow: 1 }}></div>
+              </div>
+            </div>
+          </div>
+          <div className="box-root padding-top--24 flex-flex flex-direction--column" style={{ flexGrow: 1, zIndex: 9 }}>
+            <div className="box-root padding-top--48 padding-bottom--24 flex-flex flex-justifyContent--center">
+              <h1>urlShortner</h1>
+            </div>
+            <div className="formbg-outer">
+              <div className="formbg">
+                <div className="formbg-inner padding-horizontal--48">
+                  <span className="padding-bottom--15 coloriot">Enter Your Url:</span>
+                  <form id="stripe-login" onSubmit={submitForm}>
+                    <div className="field padding-bottom--24">
+                  
+                      <input type="text" name="text" value={url} onChange={(e) => setUrl(e.target.value)}/>
+                    </div>
+                    <div className="field padding-bottom--24">
+                      <div className="">
+                        <label htmlFor="password"  style={{textAlign : "center", fontWeight : 600, color : "#212d63"}} className='coloriot'>ShortUrl</label>
+                        <div className="reset-pass">
+                       
+                        </div>
+                      </div>
+                      <input type="text" name="password" value={shorturl} readOnly/>
+                    </div>
+                    <div className="field padding-bottom--24">
+                    
+                      <input type="submit" name="submit" value="Shorten" disabled={isLoading}/>
+                    </div>
+                    <div className="field">
+                      <a className="ssolink" href="#">urlShortner always got you</a>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/api/fetchUrl.jsx
@@ -0,0 +1,5 @@
+import axios from "axios"
+export const url = axios.create({
+    baseURL : "https://urlshortner-dtin.onrender.com",
+    withCredentials : false
+})
